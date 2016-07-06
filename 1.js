06364dbde61699ea71de353f7bab5e02f347c7ae
nizp <nzp890524@163.com>
2016-07-06 15:40:18 +0800

    down

--- 1.js
+++ 1.js
@@ -4,5 +4,16 @@ function Drag(id){
 	this.disY = 0;
 }
 Drag.prototype.fnDown = function(){
-	 
+	var _this = this;
+	this.obj.addEventListener('mousedown',function(ev){
+		_this.disX = ev.pageX - this.obj.offsetLeft;
+		_this.disY = ev.pageY - this.obj.offsetTop;
+		
+		
+		document.addEventListener('mousemove',move);
+		
+		function move(ev){
+			_this.fnMove(ev);
+		}
+	});
 };
